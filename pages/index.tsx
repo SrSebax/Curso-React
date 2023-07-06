93018c1de3996b9df292a1774582d9e04a1db1be
SebasLon <111817271+SrSebax@users.noreply.github.com>
2023-07-05 20:37:20 -0500

    Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,39 @@
-import { NextPage } from "next";
 import { RandomFox } from "../components/RandomFox";
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
 //* Generate a random function between 1 and 123
-const random = () => Math.floor(Math.random() * 123)+ 1;
+const random = () => Math.floor(Math.random() * 123) + 1;
 
 //* Type Array
-type ImageItem = {id: string; url: string};
+type ImageItem = { id: string; url: string };
 
 //* Generate simple unique identifier
-const generateId = () => Math.random().toString(36).substring(2,9);
-
+const generateId = () => Math.random().toString(36).substring(2, 9);
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItem>>([
-    { id: generateId() , url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId() , url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId() , url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId() , url: `https://randomfox.ca/images/${random()}.jpg`},
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    const target = event.target
+
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+    setImages([...images, newImageItem]);
+  };
 
   return (
     <div>
       <title>Curso React.js</title>
       <main>
         <h1 className="text-3xl font-bold underline">Random Fox</h1>
-        {images.map(( {id, url }) => (
+        <button onClick={addNewFox}>Agregar imagen</button>
+        {images.map(({ id, url }) => (
           <div key={id} className="p-4">
-            <RandomFox image={url}/>
+            <RandomFox image={url} />
           </div>
         ))}
       </main>
